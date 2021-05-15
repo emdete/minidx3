45084c60b5e0cadd08434da6298fc04ca334957b
M. Dietrich <mdt@emdete.de>
2021-05-15 19:28:06 +0200

    classified

--- minidx3.py
+++ minidx3.py
@@ -5,124 +5,132 @@ from serial import Serial
 
 debug = False
 
-def check_crc(buf):
-	crc = 0
-	for b in buf[2:-3]:
-		crc ^= b
-	return crc
+class MiniDX3(Serial):
+	def __init__(self, filename, pin='0000'):
+		super(MiniDX3, self).__init__(port=filename, baudrate=19200, )
+		self.rtscts = False
+		self.xonxoff = False
+		self.timeout = 1
+		self.login(pin)
 
-def size(buf):
-	return [len(buf) & 0xff00 >> 8, len(buf) & 0x00ff, ]
+	@staticmethod
+	def __check_crc(buf):
+		crc = 0
+		for b in buf[2:-3]:
+			crc ^= b
+		# TODO
+		# print('---', int(buf[-3:-1].decode(), 16), crc, )
+		return crc
 
-def send_receive(device, buf, size=5):
-	buf.insert(0, 2)
-	buf.append(13)
-	buf = bytes(buf)
-	if debug: print(repr(buf))
-	device.write(buf)
-	if debug: sleep(.3)
-	buf = device.read(size)
-	if debug: print(repr(buf))
-	if not buf or buf[0] != 2 or buf[-1] != 13:
-		raise Exception('Protocol error in result "{}"'.format(buf))
-	return buf
+	@staticmethod
+	def __size(buf):
+		return [len(buf) & 0xff00 >> 8, len(buf) & 0x00ff, ]
 
-# L - login
-def login(device, pin='0000'):
-	buf = [ord(c) for c in pin]
-	buf.insert(0, ord('L'))
-	return send_receive(device, buf)[1] == ord('A')
+	def send_receive(self, buf, size=5):
+		buf.insert(0, 2)
+		buf.append(13)
+		buf = bytes(buf)
+		if debug: print(repr(buf))
+		self.write(buf)
+		if debug: sleep(.3)
+		buf = self.read(size)
+		if debug: print(repr(buf))
+		if not buf or buf[0] != 2 or buf[-1] != 13:
+			raise Exception('Protocol error in result "{}"'.format(buf))
+		return buf
 
-# O - logout
-def logout(device):
-	num = [ord('O'), ]
-	return send_receive(device, num)[1] == ord('A')
+	# L - login
+	def login(self, pin='0000'):
+		buf = [ord(c) for c in pin]
+		buf.insert(0, ord('L'))
+		return self.send_receive(buf)[1] == ord('A')
 
-# P - set password
-def set_password(device, pin):
-	buf = [ord(c) for c in pin]
-	buf.insert(0, ord('P'))
-	return send_receive(device, buf)[1] == ord('A')
+	# O - logout
+	def logout(self):
+		num = [ord('O'), ]
+		return self.send_receive(num)[1] == ord('A')
 
-# B - get register
-def get_register(device, no):
-	no = [ord(c) for c in '{:02x}'.format(no)]
-	#no = [no >> 8, no & 0xff, ]
-	buf = [ord('B'), ] + no
-	buf = send_receive(device, buf, 7)
-	if buf[1] == ord('A'):
-		check_crc(buf)
-		return int(buf[2:-3].decode(), 16)
+	# P - set password
+	def set_password(self, pin):
+		buf = [ord(c) for c in pin]
+		buf.insert(0, ord('P'))
+		return self.send_receive(buf)[1] == ord('A')
 
-# C - set register
-def set_register(device, no, value):
-	no = [ord(c) for c in '{:02x}'.format(no)]
-	buf = [ord('C'), ]
+	# B - get register
+	def get_register(self, no):
+		no = [ord(c) for c in '{:02x}'.format(no)]
+		#no = [no >> 8, no & 0xff, ]
+		buf = [ord('B'), ] + no
+		buf = self.send_receive(buf, 7)
+		if buf[1] == ord('A'):
+			MiniDX3.__check_crc(buf)
+			return int(buf[2:-3].decode(), 16)
 
-# F - get product version
-def get_product_version(device):
-	buf = [ord('F'), ]
-	buf = send_receive(device, buf, 100)
-	if buf[1] == ord('A'):
-		check_crc(buf)
-		# unclean binary zeros, unclear =, crc?
-		return buf[3:-4].strip(b'\00').decode().split('\r')
+	# C - set register
+	def set_register(self, no, value):
+		no = [ord(c) for c in '{:02x}'.format(no)]
+		buf = [ord('C'), ]
+		# TODO
 
-# S - set date
-def set_date(device):
-	date = datetime.now().strftime('%Y%m%d%H%M%u')
-	buf = [ord(c) for c in date]
-	buf.insert(0, ord('S'))
-	return send_receive(device, buf)[1] == ord('A')
+	# F - get product version
+	def get_product_version(self):
+		buf = [ord('F'), ]
+		buf = self.send_receive(buf, 100)
+		if buf[1] == ord('A'):
+			MiniDX3.__check_crc(buf) # unclean binary zeros, unclear =, crc?
+			return buf[3:-4].strip(b'\00').decode().split('\r')
 
-# T - get time and date
-def get_date(device):
-	buf = [ord('T'), ]
-	buf = send_receive(device, buf, 18)
-	if buf[1] == ord('A'):
-		# crc included?
-		check_crc(buf)
-		return buf[2:-3].decode()
+	# S - set date
+	def set_date(self):
+		date = datetime.now().strftime('%Y%m%d%H%M%u')
+		buf = [ord(c) for c in date]
+		buf.insert(0, ord('S'))
+		return self.send_receive(buf)[1] == ord('A')
 
-# N - get number of records
-def get_number_of_records(device):
-	buf = [ord('N'), ]
-	buf = send_receive(device, buf, 9)
-	if buf[1] == ord('A'):
-		check_crc(buf)
-		return int(buf[2:-3].decode(), 16)
+	# T - get time and date
+	def get_date(self):
+		buf = [ord('T'), ]
+		buf = self.send_receive(buf, 18)
+		if buf[1] == ord('A'):
+			MiniDX3.__check_crc(buf) # crc included?
+			return buf[2:-3].decode()
 
-# G - get record
-def get_record(device, no):
-	no = [ord(c) for c in '{:04x}'.format(no)]
-	#no = [no >> 8, no & 0xff, ]
-	buf = [ord('G'), ] + no
-	buf = send_receive(device, buf, 200)
-	if buf[1] == ord('A'):
-		return buf[2:-1] # .decode().split('?')
+	# N - get number of records
+	def get_number_of_records(self):
+		buf = [ord('N'), ]
+		buf = self.send_receive(buf, 9)
+		if buf[1] == ord('A'):
+			MiniDX3.__check_crc(buf)
+			return int(buf[2:-3].decode(), 16)
 
-# E - erase records
-def erase_records(device):
-	buf = [ord('E'), ]
-	return send_receive(device, buf)[1] == ord('A')
+	# G - get record
+	def get_record(self, no):
+		no = [ord(c) for c in '{:04x}'.format(no)]
+		#no = [no >> 8, no & 0xff, ]
+		buf = [ord('G'), ] + no
+		buf = self.send_receive(buf, 200)
+		if buf[1] == ord('A'):
+			return buf[2:-1].decode() # .split('?')
 
-with Serial(port='/dev/ttyUSB0', baudrate=int(19200), ) as device:
-	device.rtscts = False
-	device.xonxoff = False
-	device.timeout = 1
-	print('login', login(device), )
-	if debug:
-		print('get_product_version', get_product_version(device), )
-	if debug:
-		for idx in range(256):
-			print('get_register', idx, get_register(device, idx), )
-	print('set_date', set_date(device), )
-	if debug:
-		print('get_date', get_date(device), )
-	no = get_number_of_records(device)
-	print('get_number_of_records', no, )
-	for idx in range(no):
-		print('get_record', idx, get_record(device, idx), )
-	#print('erase_records', erase_records(device), )
-	print('logout', logout(device), )
+	# E - erase records
+	def erase_records(self):
+		buf = [ord('E'), ]
+		return self.send_receive(buf)[1] == ord('A')
+
+if __name__ == '__main__':
+	with MiniDX3('/dev/ttyUSB0', '0000') as device:
+		if debug:
+			print('get_product_version', device.get_product_version(), )
+		if debug:
+			for idx in range(256):
+				print('get_register', idx, device.get_register(idx), )
+		print('set_date', device.set_date(), )
+		if debug:
+			print('get_date', device.get_date(), )
+		no = device.get_number_of_records()
+		print('get_number_of_records', no, )
+		for idx in range(no):
+			print('get_record', idx, device.get_record(idx), )
+		print('erase_records', device.erase_records(), )
+		print('logout', device.logout(), )
 
