199ab920872477bf1eb35f1963739cf7163ac066
M. Dietrich <mdt@emdete.de>
2021-05-15 20:38:50 +0200

    improve protocol parsing

--- minidx3.py
+++ minidx3.py
@@ -23,7 +23,7 @@ class MiniDX3(Serial): # pylint: disable=too-many-ancestors
 			crc ^= b
 		# TODO check crc
 		# print('---', int(buf[-3:-1].decode(), 16), crc, )
-		return crc
+		return buf[0:-2]
 
 	@staticmethod
 	def __size(buf):
@@ -40,24 +40,27 @@ class MiniDX3(Serial): # pylint: disable=too-many-ancestors
 		if DEBUG: print(repr(buf))
 		if not buf or buf[0] != 2 or buf[-1] != 13:
 			raise Exception('Protocol error in result "{}"'.format(buf))
+		if buf[1] != ord('A'):
+			raise Exception('Error {}'.format(int(buf[2:4].decode(), 16)))
+		buf = buf[2:-1]
 		return buf
 
 	# L - login
 	def login(self, pin='0000'):
 		buf = [ord(c) for c in pin]
 		buf.insert(0, ord('L'))
-		return self.send_receive(buf)[1] == ord('A')
+		return self.send_receive(buf)
 
 	# O - logout
 	def logout(self):
 		num = [ord('O'), ]
-		return self.send_receive(num)[1] == ord('A')
+		return self.send_receive(num)
 
 	# P - set password
 	def set_password(self, pin):
 		buf = [ord(c) for c in pin]
 		buf.insert(0, ord('P'))
-		return self.send_receive(buf)[1] == ord('A')
+		return self.send_receive(buf)
 
 	# B - get register
 	def get_register(self, no):
@@ -65,9 +68,8 @@ class MiniDX3(Serial): # pylint: disable=too-many-ancestors
 		#no = [no >> 8, no & 0xff, ]
 		buf = [ord('B'), ] + no
 		buf = self.send_receive(buf, 7)
-		if buf[1] == ord('A'):
-			MiniDX3.__check_crc(buf)
-			return int(buf[2:-3].decode(), 16)
+		buf = MiniDX3.__check_crc(buf)
+		return int(buf.decode(), 16)
 
 	# C - set register
 	def set_register(self, no, value):
@@ -79,32 +81,29 @@ class MiniDX3(Serial): # pylint: disable=too-many-ancestors
 	def get_product_version(self):
 		buf = [ord('F'), ]
 		buf = self.send_receive(buf, 100)
-		if buf[1] == ord('A'):
-			MiniDX3.__check_crc(buf) # unclean binary zeros, unclear =, crc?
-			return buf[3:-4].strip(b'\00').decode().split('\r')
+		buf = MiniDX3.__check_crc(buf) # unclean binary zeros, unclear =, crc?
+		return buf.strip(b'\00').decode().split('\r')
 
 	# S - set date
 	def set_date(self):
 		date = datetime.now().strftime('%Y%m%d%H%M%u')
 		buf = [ord(c) for c in date]
 		buf.insert(0, ord('S'))
-		return self.send_receive(buf)[1] == ord('A')
+		return self.send_receive(buf)
 
 	# T - get time and date
 	def get_date(self):
 		buf = [ord('T'), ]
 		buf = self.send_receive(buf, 18)
-		if buf[1] == ord('A'):
-			MiniDX3.__check_crc(buf) # crc included?
-			return buf[2:-3].decode()
+		buf = MiniDX3.__check_crc(buf) # crc included?
+		return buf.decode()
 
 	# N - get number of records
 	def get_number_of_records(self):
 		buf = [ord('N'), ]
 		buf = self.send_receive(buf, 9)
-		if buf[1] == ord('A'):
-			MiniDX3.__check_crc(buf)
-			return int(buf[2:-3].decode(), 16)
+		buf = MiniDX3.__check_crc(buf)
+		return int(buf.decode(), 16)
 
 	# G - get record
 	def get_record(self, no):
@@ -112,13 +111,12 @@ class MiniDX3(Serial): # pylint: disable=too-many-ancestors
 		#no = [no >> 8, no & 0xff, ]
 		buf = [ord('G'), ] + no
 		buf = self.send_receive(buf, 200)
-		if buf[1] == ord('A'):
-			return buf[2:-1].decode() # .split('?')
+		return buf.decode() # .split('?')
 
 	# E - erase records
 	def erase_records(self):
 		buf = [ord('E'), ]
-		return self.send_receive(buf)[1] == ord('A')
+		return self.send_receive(buf)
 
 if __name__ == '__main__':
 	with MiniDX3('/dev/ttyUSB0', '0000') as device:
